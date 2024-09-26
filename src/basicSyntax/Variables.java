a12f13ffd775e27b87d742de7a7560ba5d206d74
katayama0916 <t.katayama.cingroup2012@gmail.com>
2024-09-26 23:20:27 +0900

    Variables.javaの実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,14 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+	 int num1=19;
+	 double num2=5.5;
+	 
+	 System.out.println("私の好きな整数は"+num1+"です。");
+	 System.out.println("私の好きな少数は"+num2+"です。");
+
+	}
+
+}
