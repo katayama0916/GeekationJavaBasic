4eded203f2ea17c1b9f6bbd3dcfdac35f523fac3
katayama0916 <t.katayama.cingroup2012@gmail.com>
2024-10-16 00:49:50 +0900

    add-geek-introduction-classの実装

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,12 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		GeekIntroduction.greeting(25, 170);
+		
+		GeekIntroduction.specialSkill(1,10);
+	}
+
+}
